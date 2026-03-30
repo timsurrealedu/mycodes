91924d5e831fb662b6aabca87125ff36dfae71c0
timsurrealedu <timothy.darmawan@binus.ac.id>
2026-03-30 23:25:38 +0700

    Resolved sync conflict in singly.c

--- Binus/Sem2/DataStructure/fastTrack/linkedlist/singly.c
+++ Binus/Sem2/DataStructure/fastTrack/linkedlist/singly.c
@@ -8,34 +8,7 @@ typedef struct{
 
 node* head = NULL, *tail = NULL;
 
-node* createNode(int value){
-    node* newNode = (node*)malloc(sizeof(node));
-    newNode->value=value;
-    newNode->next=NULL;
-    return newNode;
-}
+node* createNode (int val){
+    node* newNode;
 
-node* pushHead(int value){
-    node* newNode = createNode(value);
-    if(head==NULL){
-        head = tail = newNode;        
-    }
-    else{
-        newNode->next=head;
-        head=newNode;
-    }
-}
-
-node* pushTail(int value){
-    node* newNode = createNode(value);
-    if(tail==NULL){
-        head=tail=newNode;
-    }else{
-        tail->next=newNode;
-        tail=newNode;
-    }
-}
-
-node* pushSort(int value){
-    
 }
