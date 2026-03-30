6be17311ff866066cb7eb2d0819322c906f461c7
timsurrealedu <timothy.darmawan@binus.ac.id>
2026-03-30 22:57:21 +0700

    lanjutsingly

--- Binus/Sem2/DataStructure/fastTrack/linkedlist/singly.c
+++ Binus/Sem2/DataStructure/fastTrack/linkedlist/singly.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 
 typedef struct{
-    int val;
+    int value;
     struct node* next;
 }node;
+
+node* head = NULL, *tail = NULL;
+
+node* createNode(int value){
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->value=value;
+    newNode->next=NULL;
+    return newNode;
+}
+
+node* pushHead(int value){
+    node* newNode = createNode(value);
+    if(head==NULL){
+        head = tail = newNode;        
+    }
+    else{
+        newNode->next=head;
+        head=newNode;
+    }
+}
+
+node* pushTail(int value){
+    node* newNode = createNode(value);
+    if(tail==NULL){
+        head=tail=newNode;
+    }else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+}
+
+node* pushSort(int value){
+    
+}
