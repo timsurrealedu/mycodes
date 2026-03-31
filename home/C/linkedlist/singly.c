dee386ddd251fbbc1759731020d195e4649272ab
timsurrealedu <timothy.darmawan@binus.ac.id>
2026-03-31 11:28:53 +0700

    singlyhome

---
+++ home/C/linkedlist/singly.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+  int value;
+  struct node* next
+}node;
+
+node* head = NULL; *tail=NULL;
+
+node* createNode(int value){
+  node* newNode = (node*)malloc(sizeof(node));
+  newNode->value=value;
+  newNode->next=NULL;
+  return newNode;
+}
+
+void pushHead(int value){
+  node* newNode = createNode(value);
+  if(head==NULL){
+    head=tail=newNode;
+  }else{
+    newNode->next=head;
+    head=newNode;
+  }
+
+  void pushTail(int value){
+    node* newNode = createNode(value);
+    if(tail==NULL){
+      head=tail=newNode;
+    }
+    else{
+      tail->next=newNode;
+      tail=newNode;
+    }
+  }
+
+  void pushSort(int value){
+    node* newNode = createNode(value);
+    if(head==NULL){
+      head=tail=newNode;
+    }
+    else if(head->value>=value){
+      newNode->next=head;
+      head=newNode;
+    }
+    else if(tail->value<=value){
+      tail->next=newNode;
+      tail=newNode;
+    }
+    else{
+      node* back = head;
+      node* curr = head->next;
+      while(curr!=NULL&&curr->value<value){
+        back=curr;
+        curr=curr->next;
+      }
+      back->next=newNode;
+      newNode->next=curr;
+    }
+  }
+
+  void popHead(){
+    if(head==NULL){
+      printf("head is empty\n");
+      return;
+    }
+    node* temp = head;
+    head=head->next;
+    free(temp);
+    if(head==NULL) tail=NULL;
+  }
+
+  void popTail(){
+    if(head==NULL){
+      printf("head is empty\n");
+      return;
+    }
+    else if(head==tail){
+      free(head);
+      head = tail = NULL;
+      return;
+    }
+    node* curr = head;
+    while(curr->next!=tail){
+      curr=curr->next;
+    }
+    free(tail);
+    tail=curr;
+    tail->next=NULL;
+  }
+
+  
+
+}
